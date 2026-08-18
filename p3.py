de66ed33474c62562936465635270010f68ff53b
Palak Gurjar <palakgurjar413@gmail.com>
2026-08-18 13:32:01 -0700

    Add p3

---
+++ p3.py
@@ -0,0 +1,10 @@
+name = "alison hack 123abc"
+nm = name.capitalize()
+print(nm)
+
+list = [1, 3, "s", 4, 5]
+print(list)
+print(list[1:3])
+print(list[-4:-2])
+print(list[2:0:-1])
+print(list[-3:-5:-1])
